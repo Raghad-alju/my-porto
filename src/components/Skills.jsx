fb5459a5db8ee4a956beca1302a5f16a251dd942
raghad77 <raghad.alju@outlook.com>
2024-02-25 21:33:48 +0300

    new changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,11 +3,15 @@ import color from './../images/Untitled_Artwork.png';
 import { useInView } from "react-intersection-observer"
 import java from '../images/icons/java-svgrepo-com.png'
 import python from '../images/icons/python-127-svgrepo-com.png'
-import css from '../images/icons/css-svgrepo-com.png'
-import html from '../images/icons/html-svgrepo-com.png'
+import css from '../images/icons/css3-svgrepo-com.png'
+import html from '../images/icons/html5-svgrepo-com.png'
 import dotnet from '../images/icons/dotnet-svgrepo-com.png'
-import javaScript from '../images/icons/java-script-svgrepo-com.png'
+import javaScript from '../images/icons/nodejs-svgrepo-com.png'
 import tailwind from '../images/icons/tailwind-svgrepo-com.png'
+import react from '../images/icons/react-svgrepo-com.png'
+import skills from '../images/icons/skills-svgrepo-com.png'
+import creative from '../images/icons/creative-process-svgrepo-com.png'
+
 //import database from '../images/icons/my-porto/database-svgrepo-com(1).png'
 
 
@@ -22,19 +26,23 @@ function Skills() {
             <div className=" w-full h-auto ">
                 <div className="relative flex">
                     <img className=" w-[20rem] h-[20rem]  " src={color} />
-                    <svg class="w-48 h-48 text-gray-800 left-12 top-20  absolute" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.5" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15" />
-
-                    </svg>
-                    <div className=" w-1/2 bg-slate-50 h-44 m-24 ml-0 flex">
+                    
+                    <div class="w-48 h-48 text-gray-600 left-12 top-20  absolute" >
+                        <img src={skills} />
+                    </div>
+                    <div className=" w-1/2 h-44 m-24 ml-0 flex justify-center flex-wrap">
                                
                         <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={java}/></div>
                         <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={python}/></div>
-                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={css}/></div>
+                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img className=" p-[6px]" src={css}/></div>
                         <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={javaScript}/></div>
 
-                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={html}/></div>
+                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2 "><img className=" p-[6px]" src={html}/></div>
                         <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={dotnet}/></div>
+                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={tailwind}/></div>
+                        <div className=" w-20 h-20 bg-impBage-300 rounded-full p-2 mr-2"><img src={react}/></div>
+
+
 
                     </div>
                 </div>
@@ -43,13 +51,25 @@ function Skills() {
                 <div className="relative w-full h-80 ">
                     <img className=" w-[20rem] h-[20rem] absolute right-0 " src={color} />
                     
-                    <svg class="w-48 h-48 text-gray-800 right-20 top-20 absolute " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="14" height="20" fill="none" viewBox="0 0 14 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.5" d="M4 7a3 3 0 0 1 3-3M5 19h4m0-3c0-4.1 4-4.9 4-9A6 6 0 1 0 1 7c0 4 4 5 4 9h4Z" />
-                    </svg>
+                    <div class="w-48 h-48 text-gray-800 right-20 top-20 absolute" >
+                        <img src={creative} />
+                    </div>
                     
 
-                    <div className=" absolute top-[5rem] right-[20rem] w-1/2 bg-slate-500 h-44 ">
-                        <div>Creative thinker, by providing creitive solotions or ideas </div>
+                    <div className=" absolute top-[5rem] right-[20rem] w-1/2  h-44 ">
+                        <div className=" flex justify-center flex-wrap ">
+                            
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Creative</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">UX/UI design</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Graphic design</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Machine learning</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Web Development</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Research skills</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Maths</span>
+                            <span className="  bg-purple-400 text-2xl m-3 p-2 rounded-lg">Research skills</span>
+
+
+                            </div>
                     </div>
                 </div>
             </div>
